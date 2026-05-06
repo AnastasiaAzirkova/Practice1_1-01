032c1b3be94fc31effa0a49125e5f780f83f6616
AnastasiaAzirkova <azirkovaanastasia462@gmail.com>
2026-05-06 12:50:00 +0300

    Commit-2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+namespace PracticalWork1
+{
+    class Account
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public double Balance { get; set; }
+
+        public Account(string name, int number, double balance)
+        {
+            Name = name;
+            Number = number;
+            Balance = balance;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Счет: {Name} | №{Number} | Сумма: {Balance:F2} руб.");
+        }
+    }
+
+    }
